59d407b31fb612999e1abb86aba7da3fecd3cbe3
HerbalGreenTea <alex.andros.solovev@gmail.com>
2021-04-23 22:31:23 +0500

    добавлена загрузка аватраки в шапку бокового меню

--- presentation/src/main/java/com/example/habitnote/MainActivity.kt
+++ presentation/src/main/java/com/example/habitnote/MainActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.example.habitnote.di.modules.FragmentActivityModule
 import com.example.habitnote.di.сomponents.ViewModelComponent
 import com.example.habitnote.di.modules.viewModelModules.ViewModelListHabitModule
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_header_navigation_menu.*
+import kotlinx.android.synthetic.main.item_header_navigation_menu.view.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     companion object {
@@ -20,6 +24,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         settingViewModelsComponent()
         navigation_view.setNavigationItemSelectedListener(this)
+
+        Glide.with(applicationContext)
+                .load("https://tproger.ru/s3/uploads/2015/03/android-development.jpg")
+                .placeholder(R.drawable.ic_user_placeholder)
+                .error(R.drawable.ic_error)
+                .transform(CircleCrop())
+                .into(navigation_view.getHeaderView(0).user_photo)
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
